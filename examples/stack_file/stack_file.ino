86af25514f4e8b90bad8c8953405eea10bc918f5
AlexGyver <alexgyvershow@ya.ru>
2025-03-26 16:32:27 +0300

    Update stack_file.ino

--- examples/stack_file/stack_file.ino
+++ examples/stack_file/stack_file.ino
@@ -10,7 +10,7 @@ void setup() {
     // запись в файл
     gtl::stack_uniq<int> st;
     st.push(1);
-    st.push(2, 3, 4);
+    st.pushList(2, 3, 4);
 
     for (int i = 0; i < st.length(); i++) {
         Serial.print(st[i]);
